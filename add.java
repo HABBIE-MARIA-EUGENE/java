1cc0dd0c4f7326b085935f525cd8063f3725ec6c
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2025-10-04 12:21:47 +0530

    added

---
+++ add.java
@@ -0,0 +1,11 @@
+class add{
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 25;
+        int c = 25;
+        int d = a+b;
+        System.out.print(a+b+c);
+        System.out.print(d);
+    }
+}
---
+++ datatype.java
@@ -0,0 +1,13 @@
+class datatype{
+     public static void main(String args[])
+     {
+        int container = 14;
+        String eugene = "habbie maria eugene"; 
+        String container2 = " eugene ";
+        String container3 = eugene;
+        System.out.print(container);
+        System.out.print(container2);
+        System.out.print(container3);
+
+     }
+}
